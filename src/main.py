a881fa43b5370bdee87d41889217b511a587340e
seo-80 <80seo717123@gmail.com>
2023-06-09 09:26:43 +0900

    add ルンゲクッタ法

--- src/main.py
+++ src/main.py
@@ -6,11 +6,11 @@ def f(x):
 def f_diff(x):
     return 2*x
 class enviroment():
-    def __init__(self,var=[0.1,0.1],h=0.000001,mu0=10,f=f,f_difftmp=f_diff) -> None:
+    def __init__(self,x0=10,var=[0.1,0.1],h=0.0001,mu0=10,f=f,f_difftmp=f_diff) -> None:
         self.var=var# [x_var,y_var]
         self.h=h#　更新幅
         self.mu0=mu0#事前分布の平均
-        self.mu=mu0+np.random.randn()*np.sqrt(self.var[0])# 隠れ状態
+        self.mu=x0#mu0+np.random.randn()*np.sqrt(self.var[0])# 隠れ状態
         self.f=f
         self.f_difftmp=f_difftmp
         self.mu_record=np.array(self.mu)
@@ -18,22 +18,37 @@ class enviroment():
         return self.f(self.mu)+np.random.randn()*np.sqrt(self.var[1])
 
 class recognizer(enviroment):
-    def __init__(self) -> None:
-        super().__init__()
+    def __init__(self,x0=10,analysis_method=1,var=[0.1,0.1],h=0.0001,mu0=10,f=f,f_difftmp=f_diff) -> None:
+        super().__init__(x0=x0,var=var,h=h,mu0=mu0,f=f,f_difftmp=f_difftmp)
+        self.analysis_method=analysis_method
         
     def f_diff(self,x):
         if self.f_difftmp:
             return self.f_difftmp(x)
         else:
             return (self.f(x+0.001)-self.f(x))/0.001
+    def differential(self,x,y):
+        return ((y-f(x))*self.f_diff(x)/self.var[0]+(self.mu0-x)/self.var[1])
     def inference(self,y):
-        self.mu=self.mu+((y-f(self.mu))*self.f_diff(self.mu)/self.var[0]+(self.mu0-self.mu)/self.var[1])*self.h
-        self.mu_record=np.append(self.mu_record,self.mu)
-env=enviroment()
-env.mu=50
-rec=recognizer()
+        if self.analysis_method==0:#オイラー法
+            self.mu+=self.differential(self.mu,y)*self.h
+        elif self.analysis_method==1:#ルンゲクッタ法
+            k1 = self.differential(self.mu, y)
+            k2 = self.differential(self.mu + self.h * k1 / 2, y)
+            k3 = self.differential(self.mu + self.h * k2 / 2, y)
+            k4 = self.differential(self.mu + self.h * k3, y)
 
-n=10000
+            self.mu += self.h * (k1 + 2 * k2 + 2 * k3 + k4) / 6
+        self.mu_record=np.append(self.mu_record,self.mu)
+
+h=0.00001
+analysis_method=1
+
+env=enviroment(x0=10,h=h)
+rec=recognizer(x0=9,h=h,analysis_method=analysis_method)
+
+
+n=1000
 for i in range(n-1):
     y=env.output()
     rec.inference(y)
